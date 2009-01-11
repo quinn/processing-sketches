2529326cb0df01e1b9b262a12972d5685dfb4d79
Quinn Shanahan <q.shanahan+service@gmail.com>
2009-01-10 23:56:13 -0600

    grey spiky things

---
+++ test.rb
@@ -0,0 +1,33 @@
+require 'ruby-processing'
+
+class MySketch < Processing::App
+  attr_accessor :scaler, :direction
+  
+  def setup
+    color_mode RGB, 1.0
+    frame_rate 30
+    fill 0.8
+    
+    @scaler = 0
+    @direction = 'right'
+  end
+  
+  def draw
+    if scaler > width
+      @direction = 'left'
+    end
+    
+    if scaler < 0
+      @direction = 'right'
+    end
+    
+    if direction == 'right'
+      @scaler += 5
+    else
+      @scaler -= 5
+    end
+    quad(scaler, 5, rand(width), rand(50)+50, rand(width), rand(600)+50, rand(width), rand(50)+50)
+  end
+end
+
+MySketch.new :title => "My Sketch", :width => 800, :height => 900
