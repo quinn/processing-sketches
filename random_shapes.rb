daaac1b6cb024a251860cefb695d86578376b65e
Quinn Shanahan <q.shanahan+service@gmail.com>
2009-01-12 10:11:09 -0600

    shapes!

---
+++ random_shapes.rb
@@ -0,0 +1,52 @@
+require 'ruby-processing'
+
+class RandomShapes < Processing::App
+  attr_accessor :shape, :timeout, :rotation
+  def setup
+    frame_rate 25
+    @timeout = 0
+    @rotation = 1.5
+  end
+  
+  def draw
+    background 200
+    beginShape
+    
+    number_of_times = rand(20)
+    shape_width = rand(width)
+    shape_height = rand(height)
+    
+    if timeout == 0
+      @shape = ""
+      (rand(17)+3).times do
+        @shape += "vertex #{rand(width)}, #{rand(height)} \n"
+      end
+      
+      @timeout = 100
+      @rotation = 1.5
+      
+      log_shape
+    else
+      @timeout -= 1
+    end
+    
+    reposition
+
+    instance_eval @shape
+    endShape CLOSE
+  end
+  
+  def reposition
+    translate(width/@rotation+70, height/@rotation-120)
+    @rotation += 0.1
+    rotate(PI / @rotation)
+  end
+  
+  def log_shape
+    log = File.open 'shapes.log', 'a'
+    log << "\nCurrent Time: #{Time.now.to_s} \n"
+    log << @shape    
+  end
+end
+
+RandomShapes.new :title => "Random Shapes", :width => 500, :height => 500
