0d7b9a9e8dc19f653c50b592cc52152133fc43e8
Quinn Shanahan <q.shanahan+service@gmail.com>
2009-01-28 17:58:03 -0600

    the new processing is crap

--- lib/mixins/capture.rb
+++ lib/mixins/capture.rb
@@ -1,21 +1,20 @@
-require 'extlib/hook'
-
 module Capture
   def self.included includer
     includer.class_eval do |includer|
       load_ruby_library "control_panel"
       
-      after :setup, :load_capture_button
-      
       def load_capture_button
         control_panel do |c|
           c.button :save_image_action
         end
+        setup_after_button
       end
       
+      alias_method :setup_after_button, :setup
+      alias_method :setup, :load_capture_button
+      
       def save_image_action
-        raise self.inspect
-        save_frame("screenshots/")
+        save_frame("screenshots/#{title}-####.png")
       end
     end
   end
