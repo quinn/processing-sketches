e2175b8b94b759c397c71556d0a906db31338951
quinn <q.shanahan+service@gmail.com>
2009-03-25 08:51:40 -0500

    plaid plaid plaid

--- plaid2.rb
+++ plaid2.rb
@@ -10,17 +10,31 @@ class MySketch < Processing::App
   
   def setup
     setup_controls
-    frame_rate 20
+    frame_rate 30
     no_stroke
     smooth
+    
     @offset = -400
     @prev_fill = brights
     @rotation = radians(45)
     @translation = [0,0]
-    @left_lines = []
-    @right_lines = []
+    
+    @offm  = []
+    @poffm = []
+    @soffm = []
+    amount.times{ @offm  << 0 }
+    amount.times{ @poffm << 0 }
+    amount.times{ @soffm << 0 }
+    
+    @cyan = [0,255,255]   #if @cyan
+    @magenta = [255,0,255]   #if @magenta
+    @black = [0,0,0]       #if @black
+    @white = [255,255,255] #if @white
+    @fillings = [[@cyan,     @black],
+                 [@magenta,  @white]]
     @i = 0
-    background 255#255,100,200
+    
+    background 68, 70, 73
     text_font create_font("Arial", 10)
     draw
   end
@@ -31,69 +45,75 @@ class MySketch < Processing::App
   end
   
   def draw
-    if !@stop
-    
+    new_fill = brights
+    while (new_fill == @prev_fill)
       new_fill = brights
-      while (new_fill == @prev_fill)
-        new_fill = brights
-      end
-      @prev_fill = new_fill
-
-      @previous_thickness = @thickness
-      @thickness = rand(5) + 3
-      @offset = @offset + @previous_thickness.to_i + 2
-
-      draw_line @rotation, @translation, @offset, @thickness, new_fill
+    end
     
-      fill 255
-      rect 27,27,100,80
-      fill 0
-      text "offset is: #{@offset}\n", 32,39
-      text "thickness is: #{@thickness}\n", 32,51
-
-      if @offset > 350
-        @stop = true if @stopping
-        @offset = -400
-        @rotation = radians(-45)
-        @translation = [width/-2-100,height/2+100]
-        @stopping = true
-      end
-    elsif !@really_stopped
-      lay_lines
-    end
+    @prev_fill = new_fill
+    
+    qx = cos(@rotation).oned
+    qy = sin(@rotation).oned
+    
+    @translation = qx.switch*width, qy.switch*height
+    
+    push_matrix
+    
+    translate *@translation
+    rotate @rotation
+    # fill *new_fill
+    
+    puts @fillings.inspect
+    puts 'asdfasdf'
+    puts @fillings[0][0].inspect
+    puts @fillings[0][1].inspect
+    puts @fillings[1][0].inspect
+    puts @fillings[1][1].inspect
+    
+    fill *(@fillings[qx][qy] + [100])
+    
+    rect 0, @offm[@i]+2, P.width+300, @poffm[@i]
+    @poffm[@i] = 3#rand(100)/20.0
+    @offm[@i] += @soffm[@i]+0.8
+    @soffm[@i] = @poffm[@i]
+    
+    pop_matrix
+    # text_overlay
+    iterate
   end
   
-  def lay_lines
-    @really_stopped = true unless (!@right_lines.empty? or !@left_lines.empty?)
+  def amount; 11; end
+  
+  def iterate
+    @rotation += radians(360.0/amount)
+    cycle_amount
+  end
+  
+  def cycle_amount
     @i += 1
-    if (@i % 2 == 0)
-      pr = @left_lines.pop
-    else
-      pr = @right_lines.pop
-    end
-    pr.call if pr        
+    @i = 0 if @i == amount
+    @i
   end
   
-  def draw_line r, t, off, thickness, fc
-    arr = if @stopping
-      @right_lines << Proc.new do
-        push_matrix
-        rotate r
-        translate *t
-        fill *fc
-        rect 0, off, P.width+300, thickness
-        pop_matrix
-      end
-    else
-      @left_lines << Proc.new do
-        push_matrix
-        rotate r
-        translate *t
-        fill *fc
-        rect 0, off, P.width+300, thickness
-        pop_matrix
-      end
-    end
+  def make_offset
+    20#rand(20)+2
+  end
+  
+  def text_overlay
+    fill 255,255,255, 200
+    rect 27,27,170,80
+    fill 0
+    texting "offset is: #{@offset}\n"
+    texting "thickness is: #{@thickness}\n"
+    texting "sin is: #{sin(@rotation).oned}\n"
+    texting "cos is: #{cos(@rotation).oned}\n"
+    @text_offset = nil
+  end
+  
+  def texting str
+    @text_offset ||= 39
+    text str, 32, @text_offset
+    @text_offset += 12
   end
   
   include Capture
@@ -104,15 +124,14 @@ class MySketch < Processing::App
   
   def brights
     colors = []
-    # colors << [255,0,0]     #if @red
-    colors << [0,255,0]     #if @green
+    #colors << [255,0,0]     #if @red
+    # colors << [0,255,0]     #if @green
     # colors << [0,0,255]     #if @blue
-    colors << [0,255,255]   #if @cyan
     # colors << [255,255,0]   #if @yellow
-    colors << [255,0,255]   #if @magenta
-    #colors << [255,127,0]   #if @orange
-    # colors << [0,0,0]       #if @black
-    # colors << [255,255,255] #if @white
+    # colors << [255,127,0]   #if @orange
+    colors << [154,215,74]
+    colors << [74,160,215]
+    colors << [215,74,121]
     
     fill_with = colors.shuffle!.first
     fill_with ||= [255,255,255]
@@ -139,4 +158,18 @@ class Array
   end
 end
 
-P = MySketch.new :title => "Plaid2", :width => 500, :height => 500#, :full_screen => true
+class Float
+  def oned
+    return 0 if self <= 0
+    return 1
+  end
+end
+
+class Fixnum
+  def switch
+    return 1 if self == 0
+    return 0
+  end
+end
+
+P = MySketch.new :title => "Plaid", :width => 500, :height => 500, :full_screen => true
